6e463103ad3d00b1cdaeaa994bdc59ac8df2cbd8
impuls <impuls666@gmail.com>
2017-02-24 13:26:38 +0100

    part4

--- app.js
+++ app.js
@@ -6,36 +6,49 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 const config = require('./config/database');
 
-//Connect to database
+// Connect To Database
 mongoose.connect(config.database);
 
-//on connection
+// On Connection
 mongoose.connection.on('connected', () => {
-  console.log('Connected to database: '+config.database);
+  console.log('Connected to database '+config.database);
 });
 
-//on error
+// On Error
 mongoose.connection.on('error', (err) => {
   console.log('Database error: '+err);
 });
 
 const app = express();
+
 const users = require('./routes/users');
-//port number
+
+// Port Number
 const port = 3000;
-//CORS middleware
+
+// CORS Middleware
 app.use(cors());
 
-//Set static folder
-app.use(express.static(path.join(__dirname,'public')));
-//body parser middleware
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
 app.use(bodyParser.json());
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
 app.use('/users', users);
-//Index Router
+
+// Index Route
 app.get('/', (req, res) => {
-  res.send('Invalid endpoint');
+  res.send('Invalid Endpoint');
 });
 
-app.listen(port, ()=>{
-  console.log('Server started on port' +port);
+// Start Server
+app.listen(port, () => {
+  console.log('Server started on port '+port);
 });
--- models/user.js
+++ models/user.js
@@ -2,41 +2,49 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const config = require('../config/database');
 
-//User Schema
+// User Schema
 const UserSchema = mongoose.Schema({
-  name:{
+  name: {
     type: String
   },
   email: {
-    type:String,
+    type: String,
     required: true
   },
   username: {
-    type:String,
+    type: String,
     required: true
   },
   password: {
-    type:String,
+    type: String,
     required: true
   }
 });
 
 const User = module.exports = mongoose.model('User', UserSchema);
 
-module.exports.getUserById = function(id,callback) {
+module.exports.getUserById = function(id, callback){
   User.findById(id, callback);
 }
-module.exports.getUserByUsername = function(username,callback) {
+
+module.exports.getUserByUsername = function(username, callback){
   const query = {username: username}
-  User.findByOne(query, callback);
+  User.findOne(query, callback);
 }
 
 module.exports.addUser = function(newUser, callback){
   bcrypt.genSalt(10, (err, salt) => {
-    bcrypt.hash(newUser.password, salt, (err, hash) =>{
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
       if(err) throw err;
       newUser.password = hash;
       newUser.save(callback);
     });
+  });
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+  bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
+    if(err) throw err;
+    callback(null, isMatch);
   });
 }
